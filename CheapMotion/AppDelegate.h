2780b75e87747d4ea2d7e9c47f5bc05986f9fe4f
Ishaan Gulrajani <igul222@gmail.com>
2013-11-10 02:28:07 -0500

    Initial Commit

---
+++ CheapMotion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheapMotion
+//
+//  Created by Ishaan Gulrajani on 11/10/13.
+//  Copyright (c) 2013 Watchsend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
